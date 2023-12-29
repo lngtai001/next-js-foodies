9d7fb0cb3bf3c877f9e26c6356cb8a806891c2fc
Taiani Lange <langetaiani@gmail.com>
2023-12-29 14:27:41 +0200

    main page content

--- app/page.js
+++ app/page.js
@@ -1,14 +1,25 @@
 import Link from "next/link"
 
+import styles from './page.module.css'
+
 export default function Home() {
-  return (
+  return (<>
+    <header className={styles.header}>
+      <div className={styles.slideshow}></div>
+      <div>
+        <div className={styles.hero}>
+          <h1>NextLevel food for NextLevel foodies</h1>
+          <p>Taste & share food from all over the world</p>
+        </div>
+        <div className={styles.cta}>
+          <Link href="/community">Join the Community</Link>
+          <Link href="/meals">Explore Meals</Link>
+        </div>
+      </div>
+    </header>
     <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
-        Time to get started!
-      </h1>
-      <p><Link href="/meals" >Meals</Link></p>
-      <p><Link href="/meals/share" >Share Meal</Link></p>
-      <p><Link href="/community" >Community</Link></p>
+      
     </main>
+    </>
   );
 }
