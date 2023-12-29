5d106f17425ceda21c1c43208fe72bf91e522047
Taiani Lange <langetaiani@gmail.com>
2023-12-29 12:27:30 +0200

    update links

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,10 +1,9 @@
 import Link from "next/link"
 
-
 export default function MealsPage() {
 return <main>
     <h1>Meals</h1>
-    <Link href="/meals/burger" style={{ color: 'white', textAlign: 'center' }}>Burger</Link>
-    <Link href="/meals/salad" style={{ color: 'white', textAlign: 'center' }}>Salad</Link>
+    <p><Link href="/meals/burger" >Burger</Link></p>
+    <p><Link href="/meals/salad" >Salad</Link></p>
     </main>
 }
--- app/page.js
+++ app/page.js
@@ -6,11 +6,9 @@ export default function Home() {
       <h1 style={{ color: 'white', textAlign: 'center' }}>
         Time to get started!
       </h1>
-      <p>
-        <Link href="/meals" style={{ color: 'white', textAlign: 'center' }}>Meals</Link>
-        <Link href="/meals/share" style={{ color: 'white', textAlign: 'center' }} >Share</Link>
-        <Link href="/community" style={{ color: 'white', textAlign: 'center' }} >Community</Link>
-        </p>
+      <p><Link href="/meals" >Meals</Link></p>
+      <p><Link href="/meals/share" >Share Meal</Link></p>
+      <p><Link href="/community" >Community</Link></p>
     </main>
   );
 }
