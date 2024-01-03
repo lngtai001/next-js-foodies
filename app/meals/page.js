b60210e26fc1ff06fa0d7ebd1a32a76839238659
Taiani Lange <langetaiani@gmail.com>
2024-01-03 08:51:35 +0200

    db, loading and error states

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,16 @@
 import Link from "next/link"
 import classes from './page.module.css'
 import MealsGrid from "@/components/meals/meals-grid"
+import { getMeals } from '@/lib/meals'
+import { Suspense } from "react"
+
+function Meals() {
+    const meals = getMeals();
+    return <MealsGrid meals={meals} />
+}
 
 export default function MealsPage() {
+   
 return <>
 <header className={classes.header}>
     <h1>Delicious meals, created <span className={classes.highlight}>by you</span></h1>
@@ -12,7 +20,10 @@ return <>
     </p>
 </header>
 <main className={classes.main}>
-  <MealsGrid meals={[]} />
+    <Suspense fallback={ <p className={classes.loading}>Fetching meals...</p>}>
+    <Meals />
+    </Suspense>
+   
     </main>
     </>
 }
